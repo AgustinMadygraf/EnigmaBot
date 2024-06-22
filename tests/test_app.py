0982c57779a651aff968fbb022888261cf610aac
AgustinMadygraf <agustin.mtto.madygraf>
2024-06-22 04:15:25 -0300

    chore: Update pytest.ini to include asyncio marker

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,12 +7,52 @@ async def test_main(monkeypatch):
     class DummyConfigManager:
         def __init__(self, config_file):
             self.config = {
-                "chat_history_path": "data/context_window_telegram.json"
+                "model_path": "E:\\Model _Explorer",
+                "chat_history_path": "data/context_window_telegram.json",
+                "ram_options": {
+                    "1": "1 GB",
+                    "2": "4 GB",
+                    "3": "8 GB",
+                    "4": "16 GB"
+                },
+                "models_available": {
+                    "1 GB": ["all-MiniLM-L6-v2-f16.gguf"],
+                    "4 GB": ["replit-code-v1_5-3b-q4_0.gguf"],
+                    "8 GB": ["mistral-7b-openorca.Q4_0.gguf"],
+                    "16 GB": ["orca-2-13b.Q4_0.gguf"]
+                },
+                "system_templates": [
+                    {
+                        "mode": "ingeniero virtual",
+                        "template": "### System:\nResponde siempre en español."
+                    },
+                    {
+                        "mode": "genérico",
+                        "template": "### System:\nResponde siempre en español."
+                    }
+                ]
             }
-    
+
     class DummyChatBot:
-        def __init__(self, config):
-            pass
+        def __init__(self, config, input_func=lambda _: "1"):
+            self.config = config
+            self.input_func = input_func
+            self.ram_seleccionada = self.seleccionar_memoria_ram()
+            self.modelo_seleccionado = self.seleccionar_modelo()
+            self.model_path = config['model_path']
+            self.model = None
+            self.chat_histories = {}
+            self.monitor_task = None
+            self.system_template = self.seleccionar_system_template()
+
+        def seleccionar_memoria_ram(self):
+            return "1 GB"
+
+        def seleccionar_modelo(self):
+            return "all-MiniLM-L6-v2-f16.gguf"
+
+        def seleccionar_system_template(self):
+            return "### System:\nResponde siempre en español."
 
         async def iniciar_chat(self):
             pass
